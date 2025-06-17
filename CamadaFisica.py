a76c866cb2de6723b65064baf0721608307a9d50
pedrovale7 <pedrovaleprod@gmail.com>
2025-06-17 19:34:32 -0300

    Criação da main

--- CamadaFisica.py
+++ CamadaFisica.py
@@ -9,7 +9,7 @@ def manchester_modulation (bit_stream):
     for bit in bit_stream:
         signal.extend([bit ^ clock[0], bit ^ clock [1]]) #Sinal do bit XOR Sinal do clock
 
-    time=np.arange(len(signal)/2) #Eixo de Tempo, cada bit gera dois valores
+    time=np.arange(len(signal)) #Eixo de Tempo, cada bit gera dois valores
 
     return signal,time
 
@@ -31,3 +31,57 @@ def bipolar_modulation (bit_stream):
     time = np.arange(len(signal))   # Eixo do Tempo
 
     return signal,time
+
+
+
+def main(digital_modulation : str ,analogic_modulation: str, binary_input: str):
+
+    binary_sequence =[int(bit) for bit in binary_input]     # Transformar a sequencia de bits em inteiros
+
+    # Escolha do Metodos de Modulação Digital
+    if (digital_modulation == "NRZ-Polar"):
+        signal,time = nrz_modulation(binary_sequence)
+    elif (digital_modulation == "Manchester"):
+        signal,time = manchester_modulation(binary_sequence)
+    elif(digital_modulation == "Bipolar"):
+        signal,time = bipolar_modulation(binary_sequence)
+
+    plt.figure(figsize=(8,4))
+    plt.plot(time, signal, drawstyle="steps-pre")
+    plt.title(f"Modulação Digital {digital_modulation}")
+    plt.xlabel("Tempo")
+    plt.ylabel("Amplitude")
+    plt.xticks(np.arange(0,len(time),1))
+    for i in time:
+        plt.axvline(i,color="black", linestyle="--",linewidth=0.5)
+    plt.show()
+    plt.close()
+'''
+    # Escolhendo a modulação por Portadora
+    if (analogic_modulation == "ASK"):
+        analogic_signal=ask_modulation()
+    elif(analogic_modulation == "FSK"):
+        analogic_signal=fsk_modulation()
+    elif(analogic_modulation == "8QAM"):
+        analogic_signal=qam8_modulation()
+
+    plt.figure(figsize=(8,4))
+    plt.plot(analogic_signal)
+    plt.title(f"Sinal modulado por {analogic_modulation}")
+    plt.xlabel("Amostras")
+    plt.ylabel("Amplitude")
+    for i in time:
+        plt.axvline(i,color="black", linestyle="--",linewidth=0.5)
+    plt.show()
+    plt.close()
+              
+''' 
+
+
+
+
+
+
+
+bin="0101010001010101010101"
+main("Bipolar",None,bin)
